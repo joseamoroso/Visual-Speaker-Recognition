96c995441d6718b23956a21929235322bd750e3b
joseamoroso <50180879+joseamoroso@users.noreply.github.com>
2020-03-29 20:30:52 -0500

    segementVideos Updated

--- segmentVideos.py
+++ segmentVideos.py
@@ -9,48 +9,70 @@ Created on Mon Mar  9 01:31:45 2020
 import ffmpeg
 import pandas as pd
 import glob
-import time
 import os
 
-def cut_video(in_path,out_path,start,end):
-    in_file = ffmpeg.input(in_path)
-    (
-        ffmpeg
-        .trim(in_file,start=start,end=end)
-        .setpts ('PTS-STARTPTS')
-        .output(out_path)
-        .run()
+def cut_video(input_path, output_path, start, end):
+    input_stream = ffmpeg.input(input_path)
+
+    vid = (
+        input_stream.video
+        .trim(start=start, end=end)
+        .setpts('PTS-STARTPTS')
     )
+    aud = (
+        input_stream.audio
+        .filter_('atrim', start=start, end=end)
+        .filter_('asetpts', 'PTS-STARTPTS')
+    )
+
+    joined = ffmpeg.concat(vid, aud, v=1, a=1).node
+    output = ffmpeg.output(joined[0], joined[1], output_path)
+    output.run()
     
+modes = ["Normal","Silent","Whispered"]
 
-
-path = "AVOriginalDataset/Phrases/*" 
+path = "AVOriginalDataset\Digits\*" 
 video_folders = glob.glob(path)
 for video_folder in video_folders:
-    v_path = glob.glob(video_folder+"/*.mp4")
-    out_string = v_path[0].split("/")
+    v_path = glob.glob(video_folder+"\*.mp4")
+    out_string = v_path[0].split("\\")
     out_string = out_string[3].split("_")
     out_string = out_string[0]+"_"+out_string[4]+"_"
-    csv_paths = glob.glob(video_folder+"/*.csv")
+    csv_paths = glob.glob(video_folder+"\*.csv")
     for csv_path in csv_paths:
         path_len=len(csv_path.split('_'))
-        if path_len == 10:
+        if path_len == 9:
             data = pd.read_csv(csv_path) 
             for i in range(len(data)):
                 start = data.iloc[i][0] / 10000000
                 end = data.iloc[i][1] / 10000000
-                utterance = data.iloc[i][4]
-                out_string_f = "AVSegmentedDataset/"+ out_string + utterance + ".mp4"
-                cut_video(v_path[0],out_string_f,start,end)
-                print("Video " + out_string + utterance + ".mp4 created")
+                if "Digits" in path:
+                    utterance = "p"+str(data.iloc[i][4])
+                else:
+                    utterance = data.iloc[i][4]
+                
+                if "C01" in v_path[0]:
+                    out_string_f = r"AVSegmentedDataset\Digits\Normal\\"+ out_string + utterance + ".mp4"
+                
+                if "C02" in v_path[0]:
+                    out_string_f = "AVSegmentedDataset\\Digits\Whispered\\"+ out_string + utterance + ".mp4"
+
+                if "C03" in v_path[0]:
+                    out_string_f = "AVSegmentedDataset\\Digits\Silent\\"+ out_string + utterance + ".mp4"
+
+                cut_video(v_path,out_string_f,start,end)
+                print("Video " + out_string + utterance + ".mp4 created \n")
+
+
 
 #ELIMINA G AUDIOS
-result_video = glob.glob("AVSegmentedDataset/*")
-for elem in result_video:
-    elem2 = elem.split("/")
-    if 'g' in elem2[1]:
-        os.remove(elem)
-        print(elem + " DELETED \n")
+for mod in modes:                
+    result_video = glob.glob(r"AVSegmentedDataset\\Digits\\"+ mod +"\\*")
+    for elem in result_video:
+        elem2 = elem.split("\\")
+        if 'g' in elem2[5]:
+            os.remove(elem)
+            print(elem + " DELETED \n")
 
             
             
